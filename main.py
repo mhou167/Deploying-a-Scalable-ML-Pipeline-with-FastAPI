d056cee103ff1f6393488993918f26a6294b5f47
mhou167 <mhou167@wgu.edu>
2025-02-02 17:23:50 -0500

    project path defined

--- main.py
+++ main.py
@@ -25,6 +25,8 @@ class Data(BaseModel):
     capital_loss: int = Field(..., example=0, alias="capital-loss")
     hours_per_week: int = Field(..., example=40, alias="hours-per-week")
     native_country: str = Field(..., example="United-States", alias="native-country")
+    
+project_path = os.getcwd()
 
 path = os.path.join(project_path, "model", "encoder.pkl") # TODO: enter the path for the saved encoder DONE?
 encoder = load_model(path)
